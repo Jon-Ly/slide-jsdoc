b2e168a8b9cfcc843b561d05f787eb76a48630cd
Jonathan Ly <windevly@gmail.com>
2024-02-14 09:31:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+// const host = document.querySelector("#host");
+// const shadow = host?.attachShadow({ mode: "open" });
+// const span = document.createElement("span");
+// span.textContent = "I'm in the shadow DOM";
+// shadow?.appendChild(span);
